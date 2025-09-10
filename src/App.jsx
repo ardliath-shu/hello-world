3188b6119f5cd5804e01666c629b72780376d055
ardliath-shu <c5045540@hallam.shu.ac.uk>
2025-09-10 11:55:07 +0100

    More components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import Welcome from './Components/Welcome';
+import Students from './Components/Students';
+import Staff from './Components/Staff';
+import Courses from './Components/Courses';
+import Navbar from './Components/NavBar';
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
 
 const App = () => {
 
   return (
     <>
-    <h1>Hello World</h1>
+      <Welcome></Welcome>
+      <Students></Students>
+      <Staff></Staff>
+      <Courses></Courses>
     </>
   )
 }
---
+++ src/Components/Staff.jsx
@@ -0,0 +1,9 @@
+const Staff = () =>{
+    return(
+        <div>
+            <h2>Staff</h2>
+        </div>
+    )
+}
+
+export default Staff;
