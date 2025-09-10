a710a1fc61f268d758ebc341b28639f770333dd5
ardliath-shu <c5045540@hallam.shu.ac.uk>
2025-09-10 14:16:22 +0100

    Added data

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,9 +1,39 @@
+import {useState, useEffect} from 'react';
+
 const Courses = () =>{
-    return(
+
+
+const [courseData, setCourseData] = useState(null);
+const dataURL = './data/courses.json';
+
+useEffect(()=>{
+        fetch(dataURL)
+        .then(response => response.json())
+        .then(data =>{
+            setCourseData(data);
+            console.dir(data);
+        })
+}, [])
+
+
+return(
         <div>
-            <h2>Courses</h2>
+            <h2>Course List</h2>
+            <div className='course-list'>
+                {courseData ? (courseData.map((course, index)=>(
+                      <div key={index}>
+                                    <h3>{course.CourseTitle}</h3>
+                                    <p>{course.CourseSummary}</p>
+                      </div>
+                )
+            )):(<p>data is loading</p>)
+        }
+        
+            </div>
         </div>
     )
+
+
 }
 
 export default Courses;
